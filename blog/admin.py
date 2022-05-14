de7f27c91fa1b39a61f93a545bda3320e2331aca
Gabriel Marcelo Da Silva Franca <gabrielfranca-pdp@corporate.ad>
2022-05-14 09:26:59 -0300

    Mudança no layout da lista e inclusao de slug automatico

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,7 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "create", "updated")
+    prepopulated_fields = {"slug": ("title", )}
